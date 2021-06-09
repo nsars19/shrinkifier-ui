412c2d461f626aa8c7d344053b730433b62b5447
Nick S <nsardonini@gmail.com>
2021-06-09 16:28:46 -0400

    add section wrap around ZipDownload component for styling

--- src/components/ZipDownload/ZipDownload.js
+++ src/components/ZipDownload/ZipDownload.js
@@ -2,12 +2,14 @@ import styles from "./ZipDownload.module.scss";
 
 export default function ZipDownload({ file }) {
   return (
-    <div>
-      {file.map((url) => (
-        <a href={url} className={styles.link} key={Date.now()}>
-          {url ? "Download" : ""}
-        </a>
-      ))}
-    </div>
+    <section className={styles.dlWrap}>
+      <div>
+        {file.map((url) => (
+          <a href={url} className={styles.link} key={Date.now()}>
+            {url ? "Download" : ""}
+          </a>
+        ))}
+      </div>
+    </section>
   );
 }
