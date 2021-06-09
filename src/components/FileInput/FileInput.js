7d57fcc72775aa703f576ff12caecddcb685e4db
Nick S <nsardonini@gmail.com>
2021-06-09 15:42:14 -0400

    remove testing styles and state

--- src/components/FileInput/FileInput.js
+++ src/components/FileInput/FileInput.js
@@ -1,21 +1,15 @@
-import { useState } from "react";
-
 export default function FileInput(props) {
   const { setTotalSize, setErrorMsg, setFiles, slicedArray, clearDownload } =
     props;
-
-  const [dragged, setDragged] = useState(false);
 
   const sizeOf = (files) =>
     slicedArray(files).reduce((total, curr) => (total += curr.size), 0);
 
   const onDragOver = (e) => {
     e.preventDefault();
-    setDragged(true);
   };
 
   const onDrop = (e) => {
-    setDragged(false);
     e.preventDefault();
     const { files } = e.dataTransfer;
 
@@ -34,20 +28,7 @@ export default function FileInput(props) {
   };
 
   return (
-    <label
-      htmlFor="file-input"
-      onDragOver={onDragOver}
-      onDrop={onDrop}
-      style={{
-        height: 400,
-        width: 400,
-        border: "2px solid #404040",
-        display: "block",
-        background: dragged ? "#acfaac" : "none",
-        transform: dragged ? "scale(0.9)" : "none",
-        transition: "transform 1s ease, background 300ms ease",
-      }}
-    >
+    <label htmlFor="file-input" onDragOver={onDragOver} onDrop={onDrop}>
       <input
         id="file-input"
         onChange={onFileInput}
