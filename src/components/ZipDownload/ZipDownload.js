8cc0f6b7bc8ce9cc9cb7d48c9847d6ed50353dd4
Nick S <nsardonini@gmail.com>
2021-06-09 17:29:44 -0400

    conditionally render ZipDownload component
    rendering is dependent on the file being present

--- src/components/ZipDownload/ZipDownload.js
+++ src/components/ZipDownload/ZipDownload.js
@@ -2,7 +2,12 @@ import styles from "./ZipDownload.module.scss";
 
 export default function ZipDownload({ file }) {
   return (
-    <section className={styles.dlWrap}>
+    <section
+      className={styles.dlWrap}
+      style={{
+        display: file.length ? "grid" : "none",
+      }}
+    >
       {file.map((url) => (
         <a href={url} className={styles.link} key={Date.now()}>
           {url ? "Download" : ""}
