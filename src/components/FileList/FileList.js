07fb325b78923e1c131039a348a4ababd66ee453
Nick S <nsardonini@gmail.com>
2021-06-14 13:23:18 -0400

    implement individual file removal
    adds an icon to each file in the file list that indicates to the user that that particular file can be deleted. When the file is deleted the preprocessed filesize is recalculated. This was added in case adds an image they did not mean to, or if they add an invalid file format. This was the user doesn't have to re-add all the files, they can just remove the offending file.

--- src/components/FileList/FileList.js
+++ src/components/FileList/FileList.js
@@ -1,12 +1,29 @@
 import uniqid from "uniqid";
 import styles from "./FileList.module.scss";
+import { RiDeleteBack2Fill } from "react-icons/ri";
 
-export default function FileList({ files }) {
+export default function FileList({ files, setFiles, setTotalSize }) {
+  const sizeOf = (files) =>
+    files.reduce((total, curr) => (total += curr.size), 0);
+
+  const removeFile = (name) => {
+    return () => {
+      const newFiles = files.filter((file) => file.name !== name);
+      setFiles(newFiles);
+      setTotalSize({ pre: sizeOf(newFiles), post: 0 });
+    };
+  };
+
   return (
     <section className={styles.listWrap}>
       <ul className={styles.list}>
         {files.map(({ name }) => (
-          <li key={uniqid()}>{name}</li>
+          <li key={uniqid()}>
+            <p>{name}</p>
+            <span onClick={removeFile(name)}>
+              <RiDeleteBack2Fill />
+            </span>
+          </li>
         ))}
       </ul>
     </section>
